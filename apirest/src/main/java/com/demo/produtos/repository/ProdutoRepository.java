4ba9313fc28f8dd9a15d8485562728d98115b129
Luiz Valdiero <luiz.valdiero@hotmail.com>
2020-08-26 00:43:11 -0300

    create ProdutoResource and add get methods to search for Product in the database

--- apirest/src/main/java/com/demo/produtos/repository/ProdutoRepository.java
+++ apirest/src/main/java/com/demo/produtos/repository/ProdutoRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.demo.produtos.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto ,Long>{
-
+	
+	Produto findById(long id);
 }
---
+++ apirest/src/main/java/com/demo/produtos/resources/ProdutoResource.java
@@ -0,0 +1,30 @@
+package com.demo.produtos.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.produtos.repository.ProdutoRepository;
+import com.demo.produtos.models.Produto;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produtos/{id}")
+	public Produto listaProdutoPorId(@PathVariable(value="id") long id) {
+		return produtoRepository.findById(id);
+	}
+}
