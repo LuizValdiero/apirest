c930da681878038ea3000dfda21da3932173834b
Luiz Valdiero <luiz.valdiero@hotmail.com>
2020-08-26 01:22:55 -0300

    add Put method to Produto

--- apirest/src/main/java/com/demo/produtos/resources/ProdutoResource.java
+++ apirest/src/main/java/com/demo/produtos/resources/ProdutoResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,9 @@ public class ProdutoResource {
 	public void deletaProdutoPorId(@PathVariable(value="id") long id) {
 		produtoRepository.deleteById(id);
 	}
-	
-	
+
+	@PutMapping("/produtos")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 }
